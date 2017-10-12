dcba33b0126fdf66b00b1e75d3234db754bbea98
jhalkola <joona.halkola@hotmail.com>
2017-10-12 13:16:34 +0300

    Task finished

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,169 @@
 
 public class RomanNumerals {
-	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
+	
+	public String convertToRoman(int value) {
+		
+		String roman = "";
+		
+		int singles   = value % 10;
+		int tens      = ((value / 10)   % 10);
+		int hundreds  = ((value / 100)  % 10);
+		int thousands = ((value / 1000) % 10);
+		
+		while (thousands > 0) {
+			roman += "M";
+			thousands--;
+		}
+		
+		if      (hundreds == 9) roman += "CM"; 
+		else if (hundreds == 8) roman += "DCCC"; 
+		else if (hundreds == 7) roman += "DCC"; 
+		else if (hundreds == 6) roman += "DC"; 
+		else if (hundreds == 5) roman += "D"; 
+		else if (hundreds == 4) roman += "CD"; 
+		else if (hundreds == 3) roman += "CCC"; 
+		else if (hundreds == 2) roman += "CC";
+		else if (hundreds == 1) roman += "C";
+		
+		if      (tens == 9) roman += "XC"; 
+		else if (tens == 8) roman += "LXXX"; 
+		else if (tens == 7) roman += "LXX"; 
+		else if (tens == 6) roman += "LX"; 
+		else if (tens == 5) roman += "L"; 
+		else if (tens == 4) roman += "XL"; 
+		else if (tens == 3) roman += "XXX"; 
+		else if (tens == 2) roman += "XX";
+		else if (tens == 1) roman += "X";
+		
+		if      (singles == 9) roman += "IX"; 
+		else if (singles == 8) roman += "VIII"; 
+		else if (singles == 7) roman += "VII"; 
+		else if (singles == 6) roman += "VI"; 
+		else if (singles == 5) roman += "V"; 
+		else if (singles == 4) roman += "IV"; 
+		else if (singles == 3) roman += "III"; 
+		else if (singles == 2) roman += "II";
+		else if (singles == 1) roman += "I";
+		
+		return roman;
+		
+	}
+	
+	public int convertToInteger(String romanNum) throws RomanNumeralException {
+		
+		if (isValidRomanNumeral(romanNum)) {
+			return converter(romanNum);
+		} else {
+			throw new RomanNumeralException();
+		}
+	}
+	
+	public int converter(String roman) {
+		
+		int value = 0;
+		
+		for (int i=0; i<roman.length(); ++i) {
+			char c = roman.charAt(i);
+			int cValue = romanValueOfCharacter(c);
+			int nextValue = 0;
+			
+			if (i != roman.length()-1) {
+				char nextC = roman.charAt(i+1);
+				nextValue  = romanValueOfCharacter(nextC);
+			}
+			
+			if (cValue < nextValue) {
+				value += nextValue - cValue;
+				i++;
+			} else {
+				value += cValue;
+			}
+		}
+		
+		return value;
+		
+	}
+	
+	public int romanValueOfCharacter(char c) {
+		switch (c) {
+		case 'I': return 1;
+		case 'V': return 5;
+		case 'X': return 10;
+		case 'L': return 50;
+		case 'C': return 100;
+		case 'D': return 500;
+		case 'M': return 1000;
+		default:  return 0;
+		}
+	}
+	
+	public boolean isValidRomanNumeral(String roman) {
+		
+		if (!hasValidCharacters(roman))    return false;
+		if (!hasValidRepetition(roman))    return false;
+		if (!hasValidSubstractions(roman)) return false;
+		
+		return true;
+		
+	}
+	
+	public boolean hasValidCharacters(String roman) {
+		String regex = "[IVXLCDM]+";
+		return roman.matches(regex);
+	}
+	
+	public boolean hasValidRepetition(String roman) {
+
+		if (roman.contains("IIII")) return false;
+		if (roman.contains("VV"))   return false;
+		if (roman.contains("XXXX")) return false;
+		if (roman.contains("LL"))   return false;
+		if (roman.contains("CCCC")) return false;
+		if (roman.contains("DD"))   return false;
+		if (roman.contains("MMMM")) return false;
+		
+		return true;
+	}
+	
+	public boolean hasValidSubstractions(String roman) {
+		
+		if (roman.contains("IIV")) return false;
+		if (roman.contains("IIX")) return false;
+		if (roman.contains("IL"))  return false;
+		if (roman.contains("IC"))  return false;
+		if (roman.contains("ID"))  return false;
+		if (roman.contains("IM"))  return false;
+		if (roman.contains("VX"))  return false;
+		if (roman.contains("VL"))  return false;
+		if (roman.contains("VC"))  return false;
+		if (roman.contains("VD"))  return false;
+		if (roman.contains("VM"))  return false;
+		if (roman.contains("XXL")) return false;
+		if (roman.contains("XXC")) return false;
+		if (roman.contains("XD"))  return false;
+		if (roman.contains("XM"))  return false;
+		if (roman.contains("LC"))  return false;
+		if (roman.contains("LD"))  return false;
+		if (roman.contains("LM"))  return false;
+		if (roman.contains("CCM")) return false;
+		if (roman.contains("DM"))  return false;
+		
+		return true;
+	}
+	
+	
+	public int characterCountInString(String str, char c) {
+		
+		int count = 0;
+		
+		for (int i=0; i<str.length(); ++i) {
+			char ci = str.charAt(i);
+			if (ci == c) {
+				count++;
+			}
+		}
+		
+		return count;
 		
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -3,10 +3,202 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestRomanNumerals {
-
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testConvertToRoman_999() {
+		RomanNumerals roman = new RomanNumerals();
+		
+		String str = roman.convertToRoman(999);
+		
+		assertEquals("Numeral converted wrong", "CMXCIX", str);
+	}
+	
+	@Test
+	public void testConvertToRoman_3888() {
+		RomanNumerals roman = new RomanNumerals();
+		
+		String str = roman.convertToRoman(3888);
+		
+		assertEquals("Numeral converted wrong", "MMMDCCCLXXXVIII", str);
+	}
+	
+	@Test
+	public void testConvertToInt_allValidValues() throws RomanNumeralException {
+		
+		RomanNumerals roman = new RomanNumerals();
+		
+		for (int i=1; i<4000; ++i) {
+			String str = roman.convertToRoman(i);
+			int value = roman.convertToInteger(str);
+			
+			assertEquals("Valid numeral discarded " + str + " " + i, i, value);
+		}
+		
+	}
+	
+	@Test (expected = RomanNumeralException.class)
+	public void testConvertToInt_throwsOnInvalid() throws RomanNumeralException {
+		
+		RomanNumerals roman = new RomanNumerals();
+		
+		String str = "ASDSAV";
+		roman.convertToInteger(str);
+		
+	}
+	
+	@Test
+	public void testIsValidNumeral_allValidValues() {
+		
+		RomanNumerals roman = new RomanNumerals();
+		
+		for (int i=1; i<4000; ++i) {
+			String str = roman.convertToRoman(i);
+			boolean value = roman.isValidRomanNumeral(str);
+			
+			assertEquals("Valid numeral discarded " + str + " " + i, true, value);
+		}
+		
+	}
+	
+	@Test
+	public void testCharacterCount_threeIs() {
+		
+		RomanNumerals roman = new RomanNumerals();
+		
+		String str = "IIFASI";
+		
+		int count = roman.characterCountInString(str, 'I');
+		
+		assertEquals("Invalid character count", 3, count);
+		
+	}
+	
+	@Test
+	public void testCharacterCount_fourXs() {
+		
+		RomanNumerals roman = new RomanNumerals();
+		
+		String str = "XGSACXZHEXX";
+		
+		int count = roman.characterCountInString(str, 'X');
+		
+		assertEquals("Invalid character count", 4, count);
+		
+	}
+	
+	@Test
+	public void testHasValidCharacters_allValidValues() {
+		RomanNumerals roman = new RomanNumerals();
+		
+		for (int i=1; i<4000; ++i) {
+			String str = roman.convertToRoman(i);
+			boolean value = roman.hasValidCharacters(str);
+			
+			assertEquals("Invalid character count " + str + " " + i, true, value);
+		}
+	}
+	
+	@Test
+	public void testHasValidCharacters_ASDSAV() {
+		RomanNumerals roman = new RomanNumerals();
+		
+		String str = "ASDSAV";
+		
+		boolean value = roman.hasValidCharacters(str);
+		
+		assertEquals("Invalid characters passed the test", false, value);
 	}
 
+	@Test
+	public void testHasValidRepetition_allValidValues() {
+
+		RomanNumerals roman = new RomanNumerals();
+		
+		for (int i=1; i<4000; ++i) {
+			String str = roman.convertToRoman(i);
+			boolean value = roman.hasValidRepetition(str);
+			
+			assertEquals("Valid numeral discarded due to repetition " + str + " " + i, true, value);
+		}
+
+	}
+	
+	@Test
+	public void testHasValidRepetition_tooManyXs() {
+		RomanNumerals roman = new RomanNumerals();
+		
+		String str = "LXXXXVII";
+		
+		boolean value = roman.hasValidRepetition(str);
+		
+		assertEquals("Invalid repetitions passed", false, value);
+	}
+	
+	@Test
+	public void testHasValidRepetition_tooManyLs() {
+		RomanNumerals roman = new RomanNumerals();
+		
+		String str = "MLLIX";
+		
+		boolean value = roman.hasValidRepetition(str);
+		
+		assertEquals("Invalid repetitions passed", false, value);
+	}
+	
+	@Test
+	public void testHasValidRepetition_tooManyEverything() {
+		RomanNumerals roman = new RomanNumerals();
+		
+		String str = "MMMMDDCCCCLLXXXXVVIIII";
+		
+		boolean value = roman.hasValidRepetition(str);
+		
+		assertEquals("Invalid repetitions passed", false, value);
+	}
+	
+	@Test
+	public void testHasValidSubstractions_allValidValues() {
+		RomanNumerals roman = new RomanNumerals();
+		
+		for (int i=1; i<4000; ++i) {
+			String str = roman.convertToRoman(i);
+			boolean value = roman.hasValidSubstractions(str);
+			
+			assertEquals("Valid numeral discarded due to substractions " + str + " " + i, true, value);
+		}
+	}
+	
+	@Test
+	public void testHasValidSubstractions_IC() {
+		RomanNumerals roman = new RomanNumerals();
+		
+		String str = "IC";
+		
+		boolean value = roman.hasValidSubstractions(str);
+			
+		assertEquals("Invalid numeral passed by substractions", false, value);
+	}
+	
+	@Test
+	public void testHasValidSubstractions_XM() {
+		RomanNumerals roman = new RomanNumerals();
+		
+		String str = "XM";
+		
+		boolean value = roman.hasValidSubstractions(str);
+			
+		assertEquals("Invalid numeral passed by substractions", false, value);
+	}
+	
+	@Test
+	public void testHasValidSubstractions_VVX() {
+		RomanNumerals roman = new RomanNumerals();
+		
+		String str = "VVX";
+		
+		boolean value = roman.hasValidSubstractions(str);
+			
+		assertEquals("Invalid numeral passed by substractions", false, value);
+	}
+	
 }
